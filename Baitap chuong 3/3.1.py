f5f57bf685ae9a2e8af96e4d817981620d644981
Nguyen Thi Khanh Vy <ntkvy.dhkl17a1hn@sv.uneti.edu.vn>
2024-10-23 22:11:47 +0700

    init project

--- Baitap chuong 3/3.1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Lấy phiên bản NumPy hiện tại
-print("Phiên bản NumPy:", np.__version__)
-
-# Hiển thị cấu hình xây dựng của NumPy
-print("Cấu hình xây dựng của NumPy:")
-np.show_config()
